ec694908cad027e25ccb1de87417ca717fd8e96a
mat-ttu7 <61214767+mat-ttu7@users.noreply.github.com>
2020-02-19 22:31:38 -0300

    Delete Calculadora.java

--- Calculadora.java
+++
@@ -1,6 +0,0 @@
-public class Calculadora {
-    public static void main (String [] args){
-        int valor1 = 2, valor2 = 4, valorDaSoma = valor1 + valor2;
-        System.out.println("O valor da soma é " + valorDaSoma + ".");
-    }
-}

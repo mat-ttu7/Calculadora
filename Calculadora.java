7e18a8dd94e10aa279d7961d70d1700bcd8f4feb
arqdsis <arqdsis@MOOLIi0109.animaedu.intranet>
2020-02-19 22:08:03 -0300

    Primeira versão da calculadora

---
+++ Calculadora.java
@@ -0,0 +1,6 @@
+public class Calculadora {
+    public static void main (String [] args){
+        int valor1 = 2, valor2 = 4, valorDaSoma = valor1 + valor2;
+        System.out.println("O valor da soma é " + valorDaSoma + ".");
+    }
+}

8c68d95650102de829875f6dca5c1738e06b494d
Matheus Costa Silva <matheus.ttu7@gmail.com>
2020-02-26 16:26:17 -0300

    Calculadora concluida

--- Calculadora.c
+++ Calculadora.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main() {
-   int valor1, valor2, valor3, valor4, valorDaSoma, valorDaSubtracao;
+   int valor1, valor2, valor3, valor4, valor5, valor6, valorDaSoma, valorDaSubtracao, valorDaMultiplicacao;
+   float valor7, valor8, valorDaDivisao;
 
    printf("Digite dois valores: ");
    scanf("%d%d", &valor1, &valor2);
@@ -11,5 +12,15 @@ int main() {
    scanf("%d%d", &valor3, &valor4);
    valorDaSubtracao = valor3 - valor4;
    printf( "O valor da subtracao e : %d\n", valorDaSubtracao);
+   
+   printf("Digite dois valores: ");
+   scanf("%d%d", &valor5, &valor6);
+   valorDaMultiplicacao = valor5 * valor6;
+   printf( "O valor da multiplicacao e : %d\n", valorDaMultiplicacao);
+   
+   printf("Digite dois valores: ");
+   scanf("%f%f", &valor7, &valor8);
+   valorDaDivisao = valor7 / valor8;
+   printf( "O valor da divisao e : %.2f\n", valorDaDivisao);
    return 0;
 }

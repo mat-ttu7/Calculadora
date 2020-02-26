e9e60ca19ccd42b6fba0f0ba8fd8add0cfc5fe44
Matheus Costa Silva <matheus.ttu7@gmail.com>
2020-02-26 16:17:40 -0300

    Calculadora com soma e subtracao

--- Calculadora.c
+++ Calculadora.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 int main() {
-   int valor1, valor2, valorDaSoma;
+   int valor1, valor2, valor3, valor4, valorDaSoma, valorDaSubtracao;
 
    printf("Digite dois valores: ");
    scanf("%d%d", &valor1, &valor2);
    valorDaSoma = valor1 + valor2;
-   printf( "O valor da soma e : %d", valorDaSoma);
+   printf( "O valor da soma e : %d\n", valorDaSoma);
+   
+   printf("Digite dois valores: ");
+   scanf("%d%d", &valor3, &valor4);
+   valorDaSubtracao = valor3 - valor4;
+   printf( "O valor da subtracao e : %d\n", valorDaSubtracao);
    return 0;
 }
